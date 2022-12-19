b24d65aca18bad39b3512656faada6980b5a2780
Cazier92 <bryce.cazier@gmail.com>
2022-12-18 21:15:38 -0800

    modify init, generateContent, add render, appendOptions, handleClick, restore functionality from code that wasn't working

--- data/storyline.js
+++ data/storyline.js
@@ -3,14 +3,16 @@ const storyline = [
         page: 0, 
         message: 'You approach an ancient castle, what could be inside?', 
         optOne: 'Go inside and explore!',
-        optTwo: "Go home... It's been a long day..."
+        optTwo: "Go home... It's been a long day...",
+        next: [1, 0]
     },
 
     {
         page: 1,
         message: 'It seems the castle is built like a maze, but parts of it are crumbling. It might be dangerous! Up ahead you find a fire-lit hall and a dark hall.',
         optOne: 'Take the well lit route?',
-        optTwo: 'Risk the dark path and hope for light at the end of the tunnel!'
+        optTwo: 'Risk the dark path and hope for light at the end of the tunnel!',
+        next: []
     },
 
 
@@ -20,6 +22,11 @@ function startPage() {
     return storyline[0]
 }
 
+function nextPage(ref) {
+    return storyline[ref]
+}
+
 export {
     startPage,
+    nextPage
 }
--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 import { startPage } from "../data/storyline.js"
+import { nextPage } from "../data/storyline.js"
 
 
 // * Constants:
@@ -9,24 +10,32 @@ const page = []
 
 
 
+
 // * Chached Element References:
 
 const messageEl = document.getElementById('message')
-const optOneP = document.getElementById('option-one')
-const optTwoP = document.getElementById('option-two')
+// const optOneP = document.getElementById('option-one')
+// const optTwoP = document.getElementById('option-two')
+// const containerOne = document.getElementById('container-one')
+// const containerTwo = document.getElementById('container-two')
+const containerBlock = document.getElementById('container-block')
 
 
 // * Event Listeners:
 
-
+// containerOne.addEventListener('click', console.log('containerOne clicked'))
+containerBlock.addEventListener('click', handleClick)
 
 // * Functions:
 
 function init() {
     generateContent()
-    messageEl.textContent = page[0].message
-    optOneP.textContent = page[0].optOne
-    optTwoP.textContent = page[0].optTwo
+    // let nextPageArr = page[0].next
+    // console.log(nextPageArr)
+    render()
+    // messageEl.textContent = page[0].message
+    // optOneP.textContent = page[0].optOne
+    // optTwoP.textContent = page[0].optTwo
 }
 
 init()
@@ -36,3 +45,37 @@ function generateContent() {
     page.push(currentPage)
 }
 
+function render() {
+    containerBlock.innerHTML = ''
+    appendOptions()
+}
+
+function appendOptions() { 
+    let containerOne = document.createElement('div')
+    containerOne.className = 'container'
+    containerOne.innerHTML = 
+        `<div id="container-one" class="container">
+            <h2>Do You:</h2>
+            <p id="option-one" class="option-text">${page[0].optOne}</p>
+        </div>`
+    containerBlock.appendChild(containerOne)
+    if (page[0].optTwo !== null) {
+        let containerTwo = document.createElement('div')
+        containerTwo.className = 'container'
+        containerTwo.innerHTML = 
+            `<div id="container-two" class="container">
+                <h2>Do You:</h2>
+                <p id="option-two" class="option-text">${page[0].optTwo}</p>
+            </div>`
+        containerBlock.appendChild(containerTwo)
+    }
+}
+
+function handleClick(evt) {
+    if (evt.target.id === 'container-one') {
+        console.log('you clicked on container-one')
+    } else if (evt.target.id === 'container-two') {
+        console.log('you clicked on container-two')
+    }
+}
+// console.log(page[0].next)
